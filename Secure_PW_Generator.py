37fa20834e93bba6449e4726e823d14f5cd34531
dmndaries <61209745+dmndaries@users.noreply.github.com>
2021-07-30 07:38:04 -0400

    Add files via upload

---
+++ Secure_PW_Generator.py
@@ -0,0 +1,8 @@
+import string
+from random import randint, choice
+
+characters = string.ascii_letters + string.punctuation + string.digits
+print(characters)
+
+password = " ".join(choice(characters) for x in range(0, 32))
+print(password)
